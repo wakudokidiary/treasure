076eb9984bee52686240b791d66f5dcda52bab14
wakudokidiary <77178772+wakudokidiary@users.noreply.github.com>
2021-03-31 21:43:05 +0900

    Add files via upload

--- treasure.js
+++ treasure.js
@@ -1,6 +1,25 @@
 $(function() {
 
+  // ここからメニュー
+  $('#bar').click(function(){
+    $('#menu').slideDown();
+  });
 
+  $('#bar').click(function(){
+    $(this).fadeOut();
+  });
+
+  $('#batsu').click(function(){
+    $('#menu').slideUp();
+  });
+
+
+
+  $('#batsu').click(function(){
+    $('#bar').fadeIn();
+  });
+
+// ここまでメニュー
 
 
 
